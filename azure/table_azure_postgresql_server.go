e050bd6d8623c67bd1a7a701aebc950022e578e6
Subhajit Kumar Mondal <subhajit@turbot.com>
2021-03-26 16:48:31 +0530

    Add table azure_postgresql_server. Closes #63

---
+++ azure/table_azure_postgresql_server.go
@@ -0,0 +1,427 @@
+package azure
+
+import (
+	"context"
+	"strings"
+
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+
+	"github.com/Azure/azure-sdk-for-go/services/postgresql/mgmt/2020-01-01/postgresql"
+)
+
+//// TABLE DEFINITION
+
+func tableAzurePostgreSqlServer(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "azure_postgresql_server",
+		Description: "Azure PostgreSQL Server",
+		Get: &plugin.GetConfig{
+			KeyColumns:        plugin.AllColumns([]string{"name", "resource_group"}),
+			Hydrate:           getPostgreSqlServer,
+			ShouldIgnoreError: isNotFoundError([]string{"ResourceNotFound", "ResourceGroupNotFound", "404", "InvalidApiVersionParameter"}),
+		},
+		List: &plugin.ListConfig{
+			Hydrate: listPostgreSqlServers,
+		},
+		Columns: []*plugin.Column{
+			{
+				Name:        "name",
+				Description: "The friendly name that identifies the server.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "id",
+				Description: "Contains ID to identify a server uniquely.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromGo(),
+			},
+			{
+				Name:        "type",
+				Description: "The resource type of the SQL server.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "version",
+				Description: "Specifies the version of the server.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.Version").Transform(transform.ToString),
+			},
+			{
+				Name:        "location",
+				Description: "The resource location.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "administrator_login",
+				Description: "Specifies the username of the Administrator for this server.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.AdministratorLogin"),
+			},
+			{
+				Name:        "byok_enforcement",
+				Description: "Status showing whether the server data encryption is enabled with customer-managed keys.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.ByokEnforcement"),
+			},
+			{
+				Name:        "earliest_restore_date",
+				Description: "Specifies the earliest restore point creation time.",
+				Type:        proto.ColumnType_TIMESTAMP,
+				Transform:   transform.FromField("ServerProperties.EarliestRestoreDate").Transform(convertDateToTime),
+			},
+			{
+				Name:        "fully_qualified_domain_name",
+				Description: "The fully qualified domain name of the server.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.FullyQualifiedDomainName"),
+			},
+			{
+				Name:        "infrastructure_encryption",
+				Description: "Status showing whether the server enabled infrastructure encryption. Possible values include: 'InfrastructureEncryptionEnabled', 'InfrastructureEncryptionDisabled'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.InfrastructureEncryption").Transform(transform.ToString),
+			},
+			{
+				Name:        "master_server_id",
+				Description: "The master server id of a replica server.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.MasterServerID"),
+			},
+			{
+				Name:        "minimal_tls_version",
+				Description: "Enforce a minimal Tls version for the server. Possible values include: 'TLS10', 'TLS11', 'TLS12', 'TLSEnforcementDisabled'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.MinimalTLSVersion").Transform(transform.ToString),
+			},
+			{
+				Name:        "public_network_access",
+				Description: "Indicates whether or not public network access is allowed for this server. Value is optional but if passed in, must be 'Enabled' or 'Disabled'. Possible values include: 'PublicNetworkAccessEnumEnabled', 'PublicNetworkAccessEnumDisabled'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.PublicNetworkAccess").Transform(transform.ToString),
+			},
+			{
+				Name:        "replica_capacity",
+				Description: "The maximum number of replicas that a master server can have.",
+				Type:        proto.ColumnType_INT,
+				Transform:   transform.FromField("ServerProperties.ReplicaCapacity"),
+			},
+			{
+				Name:        "replication_role",
+				Description: "The replication role of the server.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.ReplicationRole"),
+			},
+			{
+				Name:        "sku_capacity",
+				Description: "The scale up/out capacity, representing server's compute units.",
+				Type:        proto.ColumnType_INT,
+				Transform:   transform.FromField("Sku.Capacity"),
+			},
+			{
+				Name:        "sku_family",
+				Description: "The family of hardware.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Sku.Family"),
+			},
+			{
+				Name:        "sku_name",
+				Description: "The name of the sku. For example: 'B_Gen4_1', 'GP_Gen5_8'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Sku.Name"),
+			},
+			{
+				Name:        "sku_size",
+				Description: "The size code, to be interpreted by resource as appropriate.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Sku.Size"),
+			},
+			{
+				Name:        "sku_tier",
+				Description: "The tier of the particular SKU. Possible values include: 'Basic', 'GeneralPurpose', 'MemoryOptimized'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Sku.Tier").Transform(transform.ToString),
+			},
+			{
+				Name:        "ssl_enforcement",
+				Description: "Enable ssl enforcement or not when connect to server. Possible values include: 'Enabled', 'Disabled'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.SslEnforcement").Transform(transform.ToString),
+			},
+			{
+				Name:        "storage_profile_backup_retention_days",
+				Description: "Backup retention days for the server.",
+				Type:        proto.ColumnType_INT,
+				Transform:   transform.FromField("ServerProperties.StorageProfile.BackupRetentionDays"),
+			},
+			{
+				Name:        "storage_profile_geo_redundant_backup",
+				Description: "Indicates whether Geo-redundant is enabled, or not for server backup.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.StorageProfile.GeoRedundantBackup").Transform(transform.ToString),
+			},
+			{
+				Name:        "storage_profile_storage_auto_grow",
+				Description: "Indicates whether storage auto grow is enabled, or not.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.StorageProfile.StorageAutogrow").Transform(transform.ToString),
+			},
+			{
+				Name:        "storage_profile_storage_mb",
+				Description: "Indicates max storage allowed for a server.",
+				Type:        proto.ColumnType_INT,
+				Transform:   transform.FromField("ServerProperties.StorageProfile.StorageMB"),
+			},
+			{
+				Name:        "user_visible_state",
+				Description: "A state of a server that is visible to user. Possible values include: 'ServerStateReady', 'ServerStateDropping', 'ServerStateDisabled', 'ServerStateInaccessible'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ServerProperties.UserVisibleState").Transform(transform.ToString),
+			},
+			{
+				Name:        "private_endpoint_connections",
+				Description: "A list of private endpoint connections on a server.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ServerProperties.PrivateEndpointConnections"),
+			},
+			{
+				Name:        "firewall_rules",
+				Description: "A list of firewall rules for a server.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getPostgreSQLServerFirewallRules,
+				Transform:   transform.FromValue(),
+			},
+			{
+				Name:        "server_administrators",
+				Description: "A list of server administrators.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getPostgreSQLServerAdministrator,
+				Transform:   transform.FromValue(),
+			},
+			{
+				Name:        "server_configurations",
+				Description: "A list of configurations for a server.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getPostgreSQLServerConfigurations,
+				Transform:   transform.FromValue(),
+			},
+
+			// steampipe standard columns
+			{
+				Name:        "tags",
+				Description: ColumnDescriptionTags,
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "akas",
+				Description: ColumnDescriptionAkas,
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ID").Transform(idToAkas),
+			},
+			{
+				Name:        "title",
+				Description: ColumnDescriptionTitle,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Name"),
+			},
+
+			// azure standard columns
+			{
+				Name:        "region",
+				Description: ColumnDescriptionRegion,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Location").Transform(toLower),
+			},
+			{
+				Name:        "resource_group",
+				Description: ColumnDescriptionResourceGroup,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID").Transform(extractResourceGroupFromID),
+			},
+			{
+				Name:        "subscription_id",
+				Description: ColumnDescriptionSubscription,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID").Transform(idToSubscriptionID),
+			},
+		},
+	}
+}
+
+//// LIST FUNCTION
+
+func listPostgreSqlServers(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+
+	client := postgresql.NewServersClient(subscriptionID)
+	client.Authorizer = session.Authorizer
+
+	result, err := client.List(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	for _, server := range *result.Value {
+		d.StreamListItem(ctx, server)
+	}
+	return nil, err
+}
+
+//// HYDRATE FUNCTIONS
+
+func getPostgreSqlServer(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getPostgreSqlServer")
+
+	name := d.KeyColumnQuals["name"].GetStringValue()
+	resourceGroup := d.KeyColumnQuals["resource_group"].GetStringValue()
+
+	// Error: postgresql.ServersClient#Get: Invalid input: autorest/validation: validation failed: parameter=resourceGroupName constraint=MinLength
+	// value="" details: value length must be greater than or equal to 1
+	if len(resourceGroup) < 1 {
+		return nil, nil
+	}
+
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+
+	client := postgresql.NewServersClient(subscriptionID)
+	client.Authorizer = session.Authorizer
+
+	op, err := client.Get(ctx, resourceGroup, name)
+	if err != nil {
+		return nil, err
+	}
+
+	// In some cases resource does not give any notFound error
+	// instead of notFound error, it returns empty data
+	if op.ID != nil {
+		return op, nil
+	}
+
+	return nil, nil
+}
+
+func getPostgreSQLServerFirewallRules(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getPostgreSQLServerFirewallRules")
+	server := h.Item.(postgresql.Server)
+
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+	resourceGroupName := strings.Split(string(*server.ID), "/")[4]
+
+	client := postgresql.NewFirewallRulesClient(subscriptionID)
+	client.Authorizer = session.Authorizer
+
+	op, err := client.ListByServer(ctx, resourceGroupName, *server.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	var firewallRules []map[string]interface{}
+	for _, i := range *op.Value {
+		objectMap := make(map[string]interface{})
+		if i.ID != nil {
+			objectMap["ID"] = i.ID
+		}
+		if i.Name != nil {
+			objectMap["Name"] = i.Name
+		}
+		if i.Type != nil {
+			objectMap["Type"] = i.Type
+		}
+		if i.FirewallRuleProperties != nil {
+			objectMap["FirewallRuleProperties"] = i.FirewallRuleProperties
+		}
+		firewallRules = append(firewallRules, objectMap)
+	}
+
+	return firewallRules, nil
+}
+
+func getPostgreSQLServerAdministrator(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getPostgreSQLServerAdministrator")
+	server := h.Item.(postgresql.Server)
+
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+	resourceGroupName := strings.Split(string(*server.ID), "/")[4]
+
+	client := postgresql.NewServerAdministratorsClient(subscriptionID)
+	client.Authorizer = session.Authorizer
+
+	op, err := client.List(ctx, resourceGroupName, *server.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	var serverAdministrators []map[string]interface{}
+	for _, i := range *op.Value {
+		objectMap := make(map[string]interface{})
+		if i.ID != nil {
+			objectMap["ID"] = i.ID
+		}
+		if i.Name != nil {
+			objectMap["Name"] = i.Name
+		}
+		if i.Type != nil {
+			objectMap["Type"] = i.Type
+		}
+		if i.ServerAdministratorProperties != nil {
+			objectMap["ServerAdministratorProperties"] = i.ServerAdministratorProperties
+		}
+		serverAdministrators = append(serverAdministrators, objectMap)
+	}
+	return serverAdministrators, nil
+}
+
+func getPostgreSQLServerConfigurations(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getPostgreSQLServerConfigurations")
+	server := h.Item.(postgresql.Server)
+
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+	resourceGroupName := strings.Split(string(*server.ID), "/")[4]
+
+	client := postgresql.NewConfigurationsClient(subscriptionID)
+	client.Authorizer = session.Authorizer
+
+	op, err := client.ListByServer(ctx, resourceGroupName, *server.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	var serverParameters []map[string]interface{}
+	for _, i := range *op.Value {
+		objectMap := make(map[string]interface{})
+		if i.ID != nil {
+			objectMap["ID"] = i.ID
+		}
+		if i.Name != nil {
+			objectMap["Name"] = i.Name
+		}
+		if i.Type != nil {
+			objectMap["Type"] = i.Type
+		}
+		if i.ConfigurationProperties != nil {
+			objectMap["ConfigurationProperties"] = i.ConfigurationProperties
+		}
+		serverParameters = append(serverParameters, objectMap)
+	}
+	return serverParameters, nil
+}
---
+++ docs/tables/azure_postgresql_server.md
@@ -0,0 +1,81 @@
+# Table: azure_postgresql_server
+
+Azure Database for PostgreSQL is a relational database service based on the open-source Postgres database engine. It's a fully managed database-as-a-service that can handle mission-critical workloads with predictable performance, security, high availability, and dynamic scalability.
+
+## Examples
+
+### List servers for which Enforce SSL connection is enabled
+
+```sql
+select
+  name,
+  id,
+  location,
+  ssl_enforcement
+from
+  azure_postgresql_server
+where
+  ssl_enforcement = 'Enabled';
+```
+
+### List servers for which 'Allow access to Azure services' feature is disabled
+
+```sql
+select
+  name,
+  id,
+  rule ->> 'Name' as rule_name,
+  rule ->> 'Type' as rule_type,
+  rule -> 'FirewallRuleProperties' ->> 'endIpAddress' as end_ip_address,
+  rule -> 'FirewallRuleProperties' ->> 'startIpAddress' as start_ip_address
+from
+  azure_postgresql_server,
+  jsonb_array_elements(firewall_rules) as rule
+where
+  rule ->> 'Name' = 'AllowAllAzureIps'
+  and rule -> 'FirewallRuleProperties' -> 'startIpAddress' = '0.0.0.0'
+  and rule -> 'FirewallRuleProperties' -> 'endIpAddress' = '0.0.0.0';
+```
+
+## List servers for which Active Directory Admin is not configured
+
+```sql
+select
+  name,
+  id,
+  location
+from
+  azure_postgresql_server
+where
+  server_administrators is null;
+```
+
+### List servers for which 'log_checkpoints' is set to 'OFF'
+
+```sql
+select
+  name,
+  configurations ->> 'Name' as configuration_name,
+  configurations -> 'ConfigurationProperties' ->> 'value' as configuration_value
+from
+  azure_postgresql_server,
+  jsonb_array_elements(server_configurations) as configurations
+where
+  configurations ->> 'Name' = 'log_checkpoints'
+  and configurations -> 'ConfigurationProperties' ->> 'value' = 'OFF';
+```
+
+### List servers for which 'log_retention_days' is greater than 3 days
+
+```sql
+select
+  name,
+  configurations ->> 'Name' as configuration_name,
+  configurations -> 'ConfigurationProperties' ->> 'value' as configuration_value
+from
+  azure_postgresql_server,
+  jsonb_array_elements(server_configurations) as configurations
+where
+  configurations ->> 'Name' = 'log_retention_days'
+  and (configurations -> 'ConfigurationProperties' ->> 'value')::INTEGER > 3;
+```
