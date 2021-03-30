f8f3dce06cf164c7f297ceb31eed399e9845b2a9
bigdatasourav <bigdatasourav@gmail.com>
2021-03-30 19:08:21 +0530

    required tables

---
+++ azure/table_azure_diagnostic_setting.go
@@ -0,0 +1,133 @@
+package azure
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/2020-09-01/monitor/mgmt/insights"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+)
+
+//// TABLE DEFINITION ////
+
+func tableAzureDiagnosticSetting(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "azure_diagnostic_setting",
+		Description: "Azure Diagnostic Setting",
+		Get: &plugin.GetConfig{
+			KeyColumns:        plugin.SingleColumn("name"),
+			Hydrate:           getKeyDiagnosticSetting,
+			ShouldIgnoreError: isNotFoundError([]string{"ResourceNotFound", "ResourceGroupNotFound", "404"}),
+		},
+		List: &plugin.ListConfig{
+			Hydrate: listKeyDiagnosticSetting,
+		},
+		Columns: []*plugin.Column{
+			{
+				Name:        "name",
+				Type:        proto.ColumnType_STRING,
+				Description: "The name of the resource.",
+			},
+			{
+				Name:        "id",
+				Description: "The resource Id.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromGo(),
+			},
+			{
+				Name:        "type",
+				Description: "Type of the resource",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "diagnostic_settings",
+				Description: "Properties of a Diagnostic Settings Resource.",
+				Type:        proto.ColumnType_JSON,
+			},
+
+			// Standard columns
+			{
+				Name:        "title",
+				Description: ColumnDescriptionTitle,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Name"),
+			},
+			{
+				Name:        "akas",
+				Description: ColumnDescriptionAkas,
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ID").Transform(idToAkas),
+			},
+			{
+				Name:        "resource_group",
+				Description: ColumnDescriptionResourceGroup,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID").Transform(extractResourceGroupFromID),
+			},
+			{
+				Name:        "subscription_id",
+				Description: ColumnDescriptionSubscription,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID").Transform(idToSubscriptionID),
+			},
+		},
+	}
+}
+
+//// FETCH FUNCTIONS ////
+
+func listKeyDiagnosticSetting(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	session, err := GetNewSession(ctx, d.ConnectionManager, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+
+	diagnosticSettingClient := insights.NewDiagnosticSettingsClient(subscriptionID)
+	diagnosticSettingClient.Authorizer = session.Authorizer
+
+	resourceURI := "/subscriptions/" + subscriptionID
+	result, err := diagnosticSettingClient.List(ctx, resourceURI)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, diagnosticSetting := range *result.Value {
+		d.StreamListItem(ctx, diagnosticSetting)
+	}
+
+	return nil, err
+}
+
+//// HYDRATE FUNCTIONS ////
+
+func getKeyDiagnosticSetting(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getKeyDiagnosticSetting")
+
+	name := d.KeyColumnQuals["name"].GetStringValue()
+
+	session, err := GetNewSession(ctx, d.ConnectionManager, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+
+	diagnosticSettingClient := insights.NewDiagnosticSettingsClient(subscriptionID)
+	diagnosticSettingClient.Authorizer = session.Authorizer
+
+	resourceURI := "/subscriptions/" + subscriptionID
+	op, err := diagnosticSettingClient.Get(ctx, resourceURI, name)
+	if err != nil {
+		return nil, err
+	}
+
+	// In some cases resource does not give any notFound error
+	// instead of notFound error, it returns empty data
+	if op.ID != nil {
+		return op, nil
+	}
+
+	return nil, nil
+}
---
+++ azure/table_azure_log_profile.go
@@ -0,0 +1,172 @@
+package azure
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/2020-09-01/monitor/mgmt/insights"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+)
+
+//// TABLE DEFINITION ////
+
+func tableAzureLogProfile(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "azure_log_profile",
+		Description: "Azure Log Profile",
+		Get: &plugin.GetConfig{
+			KeyColumns:        plugin.SingleColumn("name"),
+			Hydrate:           getKeyLogProfile,
+			ShouldIgnoreError: isNotFoundError([]string{"ResourceNotFound", "ResourceGroupNotFound", "404"}),
+		},
+		List: &plugin.ListConfig{
+			Hydrate: listKeyLogProfile,
+		},
+		Columns: []*plugin.Column{
+			{
+				Name:        "name",
+				Type:        proto.ColumnType_STRING,
+				Description: "The name of the resource.",
+			},
+			{
+				Name:        "id",
+				Description: "The resource Id.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromGo(),
+			},
+			{
+				Name:        "type",
+				Description: "Type of the resource",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "location",
+				Description: "Resource location.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "storage_account_id",
+				Description: "the resource id of the storage account to which you would like to send the Activity Log.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("LogProfileProperties.StorageAccountID"),
+			},
+			{
+				Name:        "service_bus_rule_id",
+				Description: "The service bus rule ID of the service bus namespace in which you would like to have Event Hubs created for streaming the Activity Log.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("LogProfileProperties.ServiceBusRuleID"),
+			},
+			{
+				Name:        "log_event_location",
+				Description: "List of regions for which Activity Log events should be stored or streamed.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("LogProfileProperties.Locations"),
+			},
+			{
+				Name:        "categories",
+				Description: "The categories of the logs.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("LogProfileProperties.Categories"),
+			},
+			{
+				Name:        "retention_policy",
+				Description: "The retention policy for the events in the log.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("LogProfileProperties.RetentionPolicy"),
+			},
+
+			// Standard columns
+			{
+				Name:        "title",
+				Description: ColumnDescriptionTitle,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Name"),
+			},
+			{
+				Name:        "tags",
+				Description: ColumnDescriptionTags,
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "akas",
+				Description: ColumnDescriptionAkas,
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ID").Transform(idToAkas),
+			},
+			{
+				Name:        "region",
+				Description: ColumnDescriptionRegion,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Location").Transform(toLower),
+			},
+			{
+				Name:        "resource_group",
+				Description: ColumnDescriptionResourceGroup,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID").Transform(extractResourceGroupFromID),
+			},
+			{
+				Name:        "subscription_id",
+				Description: ColumnDescriptionSubscription,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID").Transform(idToSubscriptionID),
+			},
+		},
+	}
+}
+
+//// FETCH FUNCTIONS ////
+
+func listKeyLogProfile(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	session, err := GetNewSession(ctx, d.ConnectionManager, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+
+	logProfileClient := insights.NewLogProfilesClient(subscriptionID)
+	logProfileClient.Authorizer = session.Authorizer
+
+	result, err := logProfileClient.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, logProfile := range *result.Value {
+		d.StreamListItem(ctx, logProfile)
+	}
+
+	return nil, err
+}
+
+//// HYDRATE FUNCTIONS ////
+
+func getKeyLogProfile(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getKeyLogProfile")
+
+	name := d.KeyColumnQuals["name"].GetStringValue()
+
+	session, err := GetNewSession(ctx, d.ConnectionManager, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+
+	logProfileClient := insights.NewLogProfilesClient(subscriptionID)
+	logProfileClient.Authorizer = session.Authorizer
+
+	op, err := logProfileClient.Get(ctx, name)
+	if err != nil {
+		return nil, err
+	}
+
+	// In some cases resource does not give any notFound error
+	// instead of notFound error, it returns empty data
+	if op.ID != nil {
+		return op, nil
+	}
+
+	return nil, nil
+}
