d746806993385445f9a680d5667c1304ad0b3449
Subhajit Kumar Mondal <subhajit@turbot.com>
2021-04-08 11:53:59 +0530

    Reverted vault table changes

--- azure/table_azure_key_vault.go
+++ azure/table_azure_key_vault.go
@@ -2,7 +2,6 @@ package azure
 
 import (
 	"context"
-	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/services/keyvault/mgmt/2019-09-01/keyvault"
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
@@ -11,7 +10,7 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/plugin"
 )
 
-//// TABLE DEFINITION
+//// TABLE DEFINITION ////
 
 func tableAzureKeyVault(_ context.Context) *plugin.Table {
 	return &plugin.Table{
@@ -41,7 +40,6 @@ func tableAzureKeyVault(_ context.Context) *plugin.Table {
 				Name:        "vault_uri",
 				Description: "Contains URI of the vault for performing operations on keys and secrets",
 				Type:        proto.ColumnType_STRING,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.VaultURI"),
 			},
 			{
@@ -53,7 +51,6 @@ func tableAzureKeyVault(_ context.Context) *plugin.Table {
 				Name:        "enabled_for_deployment",
 				Description: "Indicates whether Azure Virtual Machines are permitted to retrieve certificates stored as secrets from the key vault",
 				Type:        proto.ColumnType_BOOL,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.EnabledForDeployment"),
 				Default:     false,
 			},
@@ -61,7 +58,6 @@ func tableAzureKeyVault(_ context.Context) *plugin.Table {
 				Name:        "enabled_for_disk_encryption",
 				Description: "Indicates whether Azure Disk Encryption is permitted to retrieve secrets from the vault and unwrap keys",
 				Type:        proto.ColumnType_BOOL,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.EnabledForDiskEncryption"),
 				Default:     false,
 			},
@@ -69,7 +65,6 @@ func tableAzureKeyVault(_ context.Context) *plugin.Table {
 				Name:        "enabled_for_template_deployment",
 				Description: "Indicates whether Azure Resource Manager is permitted to retrieve secrets from the key vault",
 				Type:        proto.ColumnType_BOOL,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.EnabledForTemplateDeployment"),
 				Default:     false,
 			},
@@ -77,7 +72,6 @@ func tableAzureKeyVault(_ context.Context) *plugin.Table {
 				Name:        "enable_rbac_authorization",
 				Description: "Property that controls how data actions are authorized",
 				Type:        proto.ColumnType_BOOL,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.EnableRbacAuthorization"),
 				Default:     false,
 			},
@@ -85,7 +79,6 @@ func tableAzureKeyVault(_ context.Context) *plugin.Table {
 				Name:        "purge_protection_enabled",
 				Description: "Indicates whether protection against purge is enabled for this vault",
 				Type:        proto.ColumnType_BOOL,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.EnablePurgeProtection"),
 				Default:     false,
 			},
@@ -93,46 +86,40 @@ func tableAzureKeyVault(_ context.Context) *plugin.Table {
 				Name:        "soft_delete_enabled",
 				Description: "Indicates whether the 'soft delete' functionality is enabled for this key vault",
 				Type:        proto.ColumnType_BOOL,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.EnableSoftDelete"),
 			},
 			{
 				Name:        "soft_delete_retention_in_days",
 				Description: "Contains softDelete data retention days",
 				Type:        proto.ColumnType_INT,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.SoftDeleteRetentionInDays"),
 			},
 			{
 				Name:        "sku_family",
 				Description: "Contains SKU family name",
 				Type:        proto.ColumnType_STRING,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.Sku.Family"),
 			},
 			{
 				Name:        "sku_name",
 				Description: "SKU name to specify whether the key vault is a standard vault or a premium vault",
 				Type:        proto.ColumnType_STRING,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.Sku.Name").Transform(transform.ToString),
 			},
 			{
 				Name:        "tenant_id",
 				Description: "The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault",
 				Type:        proto.ColumnType_STRING,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.TenantID").Transform(transform.ToString),
 			},
 			{
 				Name:        "access_policies",
 				Description: "A list of 0 to 1024 identities that have access to the key vault",
 				Type:        proto.ColumnType_JSON,
-				Hydrate:     getKeyVault,
 				Transform:   transform.FromField("Properties.AccessPolicies"),
 			},
 
-			// Steampipe standard columns
+			// Standard columns
 			{
 				Name:        "title",
 				Description: ColumnDescriptionTitle,
@@ -150,8 +137,6 @@ func tableAzureKeyVault(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_JSON,
 				Transform:   transform.FromField("ID").Transform(idToAkas),
 			},
-
-			// Azure standard columns
 			{
 				Name:        "region",
 				Description: ColumnDescriptionRegion,
@@ -174,7 +159,7 @@ func tableAzureKeyVault(_ context.Context) *plugin.Table {
 	}
 }
 
-//// LIST FUNCTION
+//// FETCH FUNCTIONS ////
 
 func listKeyVaults(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	session, err := GetNewSession(ctx, d, "MANAGEMENT")
@@ -185,47 +170,42 @@ func listKeyVaults(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateDa
 
 	keyVaultClient := keyvault.NewVaultsClient(subscriptionID)
 	keyVaultClient.Authorizer = session.Authorizer
-	maxResults := int32(100)
 
-	// Pagination is not handled, as the API always sends value of NotDone() as true,
-	// and the list goes to infinite
-	result, err := keyVaultClient.List(ctx, &maxResults)
-	if err != nil {
-		return nil, err
-	}
-	for _, vault := range result.Values() {
-		d.StreamListItem(ctx, vault)
+	pagesLeft := true
+	for pagesLeft {
+		result, err := keyVaultClient.ListBySubscription(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, vault := range result.Values() {
+			d.StreamListItem(ctx, vault)
+		}
+		result.NextWithContext(context.Background())
+		pagesLeft = result.NotDone()
 	}
 
 	return nil, err
 }
 
-//// HYDRATE FUNCTIONS
+//// HYDRATE FUNCTIONS ////
 
 func getKeyVault(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	plugin.Logger(ctx).Trace("getKeyVault")
 
-	// Create session
+	name := d.KeyColumnQuals["name"].GetStringValue()
+	resourceGroup := d.KeyColumnQuals["resource_group"].GetStringValue()
+
 	session, err := GetNewSession(ctx, d, "MANAGEMENT")
 	if err != nil {
 		return nil, err
 	}
 	subscriptionID := session.SubscriptionID
 
-	var name, resourceGroup string
-	if h.Item != nil {
-		data := h.Item.(keyvault.Resource)
-		name = *data.Name
-		resourceGroup = strings.Split(*data.ID, "/")[4]
-	} else {
-		name = d.KeyColumnQuals["name"].GetStringValue()
-		resourceGroup = d.KeyColumnQuals["resource_group"].GetStringValue()
-	}
+	keyVaultClient := keyvault.NewVaultsClient(subscriptionID)
+	keyVaultClient.Authorizer = session.Authorizer
 
-	client := keyvault.NewVaultsClient(subscriptionID)
-	client.Authorizer = session.Authorizer
-
-	op, err := client.Get(ctx, resourceGroup, name)
+	op, err := keyVaultClient.Get(ctx, resourceGroup, name)
 	if err != nil {
 		return nil, err
 	}
