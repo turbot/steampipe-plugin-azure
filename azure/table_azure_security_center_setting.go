055d82d70cae5497b76ddbc37216cf3038023657
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2021-05-20 11:46:20 +0530

    Add table azure_security_center_setting. Closes #114 (#115)

---
+++ azure/table_azure_security_center_setting.go
@@ -0,0 +1,122 @@
+package azure
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/services/preview/security/mgmt/v3.0/security"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+)
+
+//// TABLE DEFINITION
+
+func tableAzureSecurityCenterSetting(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "azure_security_center_setting",
+		Description: "Azure Security Center Setting",
+		Get: &plugin.GetConfig{
+			KeyColumns: plugin.SingleColumn("name"),
+			Hydrate:    getSecurityCenterSetting,
+		},
+		List: &plugin.ListConfig{
+			Hydrate: listSecurityCenterSettings,
+		},
+		Columns: []*plugin.Column{
+			{
+				Name:        "id",
+				Type:        proto.ColumnType_STRING,
+				Description: "The resource id.",
+				Transform:   transform.FromGo(),
+			},
+			{
+				Name:        "name",
+				Description: "The resource name.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "enabled",
+				Description: "Data export setting status.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("DataExportSettingProperties.Enabled"),
+			},
+			{
+				Name:        "type",
+				Description: "The resource type.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "kind",
+				Description: "The kind of the settings string (DataExportSettings).",
+				Type:        proto.ColumnType_STRING,
+			},
+
+			// Steampipe standard columns
+			{
+				Name:        "title",
+				Description: ColumnDescriptionTitle,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Name"),
+			},
+			{
+				Name:        "akas",
+				Description: ColumnDescriptionAkas,
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ID").Transform(idToAkas),
+			},
+
+			// Azure standard columns
+			{
+				Name:        "subscription_id",
+				Description: ColumnDescriptionSubscription,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID").Transform(idToSubscriptionID),
+			},
+		},
+	}
+}
+
+//// LIST FUNCTION
+
+func listSecurityCenterSettings(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+
+	subscriptionID := session.SubscriptionID
+	settingClient := security.NewSettingsClient(subscriptionID, "")
+	settingClient.Authorizer = session.Authorizer
+
+	settingList, err := settingClient.List(ctx)
+	if err != nil {
+		return err, nil
+	}
+
+	for _, setting := range settingList.Values() {
+		d.StreamListItem(ctx, setting)
+	}
+	return nil, nil
+}
+
+//// HYDRATE FUNCTIONS
+
+func getSecurityCenterSetting(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	name := d.KeyColumnQuals["name"].GetStringValue()
+
+	subscriptionID := session.SubscriptionID
+	settingClient := security.NewSettingsClient(subscriptionID, "")
+	settingClient.Authorizer = session.Authorizer
+
+	setting, err := settingClient.Get(ctx, name)
+	if err != nil {
+		return err, nil
+	}
+
+	return setting.Value, nil
+}
---
+++ docs/tables/azure_security_center_setting.md
@@ -0,0 +1,29 @@
+# Table: azure_security_center_setting
+
+Azure security center settings contains different configurations in security center.
+
+## Examples
+
+### Basic info
+
+```sql
+select
+  id,
+  name,
+  enabled,
+from
+  azure_security_center_setting;
+```
+
+### List the enabled settings for security center
+
+```sql
+select
+  id,
+  name,
+  type
+from
+  azure_security_center_setting
+where
+  enabled;
+```
