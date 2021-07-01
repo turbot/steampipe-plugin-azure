06f054aa53b814445b871d196ed07666f0964967
bigdatasourav <bigdatasourav@gmail.com>
2021-07-01 12:29:41 +0530

    test_update

--- azure-test/tests/azure_virtual_network_gateway/test-not-found-query.sql
+++ azure-test/tests/azure_virtual_network_gateway/test-not-found-query.sql
@@ -1,3 +1,3 @@
-select name, akas, tags, title
+select name, akas, title
 from azure.azure_virtual_network_gateway
-where name = 'dummy-{{ resourceName }}' and resource_group = '{{ resourceName }}'
+where name = 'dummy-{{ resourceName }}' and resource_group = '{{ resourceName }}';
