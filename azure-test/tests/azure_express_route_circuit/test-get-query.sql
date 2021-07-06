ec7b0965d1309fa92272d9e3e993219886f923b0
bigdatasourav <bigdatasourav@gmail.com>
2021-07-06 12:12:27 +0530

    test-update

--- azure-test/tests/azure_express_route_circuit/test-get-query.sql
+++ azure-test/tests/azure_express_route_circuit/test-get-query.sql
@@ -1,3 +1,3 @@
 select name, id, sku_tier, resource_group
 from azure.azure_express_route_circuit
-where name = '{{ resourceName }}' and resource_group = '{{ resourceName }}'
+where name = '{{ resourceName }}' and resource_group = '{{ resourceName }}';
