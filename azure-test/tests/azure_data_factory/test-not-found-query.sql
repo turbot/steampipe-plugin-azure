5ccaeda02edc6517c7adb38951bfd7795bef7888
rajeshbal65 <rajesh.bal@turbot.com>
2021-07-12 10:46:16 +0530

    update

--- azure-test/tests/azure_data_factory/test-not-found-query.sql
+++
@@ -1,3 +0,0 @@
-select name, tags, title, akas
-from azure.azure_data_factory
-where name = 'dummy-{{resourceName}}' and resource_group = '{{resourceName}}'
--- azure-test/tests/azure_data_factory/test-turbot-query.sql
+++
@@ -1,3 +0,0 @@
-select name, tags, title, akas
-from azure.azure_data_factory
-where name = '{{resourceName}}' and resource_group = '{{resourceName}}'
