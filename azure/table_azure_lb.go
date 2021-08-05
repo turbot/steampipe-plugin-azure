6db28bfc4f70d62f2ab50188251106ce51b16a7e
Arnab Ghosh <arnabg57@gmail.com>
2021-08-05 16:16:35 +0530

    add diagnostic settings

--- azure/table_azure_lb.go
+++ azure/table_azure_lb.go
@@ -3,6 +3,7 @@ package azure
 import (
 	"context"
 
+	"github.com/Azure/azure-sdk-for-go/profiles/2020-09-01/monitor/mgmt/insights"
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-02-01/network"
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
@@ -87,6 +88,13 @@ func tableAzureLoadBalancer(_ context.Context) *plugin.Table {
 				Description: "Collection of backend address pools used by a load balancer.",
 				Type:        proto.ColumnType_JSON,
 				Transform:   transform.FromField("LoadBalancerPropertiesFormat.BackendAddressPools"),
+			},
+			{
+				Name:        "diagnostic_settings",
+				Description: "A list of active diagnostic settings for the load balancer.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     listLoadBalancerDiagnosticSettings,
+				Transform:   transform.FromValue(),
 			},
 			{
 				Name:        "frontend_ip_configurations",
@@ -224,4 +232,45 @@ func getLoadBalancer(ctx context.Context, d *plugin.QueryData, h *plugin.Hydrate
 	}
 
 	return nil, nil
+}
+
+func listLoadBalancerDiagnosticSettings(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("listAzureLoadBalancerDiagnosticSettings")
+	id := *h.Item.(network.LoadBalancer).ID
+
+	// Create session
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+
+	client := insights.NewDiagnosticSettingsClient(subscriptionID)
+	client.Authorizer = session.Authorizer
+
+	op, err := client.List(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	// If we return the API response directly, the output only gives
+	// the contents of DiagnosticSettings
+	var diagnosticSettings []map[string]interface{}
+	for _, i := range *op.Value {
+		objectMap := make(map[string]interface{})
+		if i.ID != nil {
+			objectMap["id"] = i.ID
+		}
+		if i.Name != nil {
+			objectMap["name"] = i.Name
+		}
+		if i.Type != nil {
+			objectMap["type"] = i.Type
+		}
+		if i.DiagnosticSettings != nil {
+			objectMap["properties"] = i.DiagnosticSettings
+		}
+		diagnosticSettings = append(diagnosticSettings, objectMap)
+	}
+	return diagnosticSettings, nil
 }
