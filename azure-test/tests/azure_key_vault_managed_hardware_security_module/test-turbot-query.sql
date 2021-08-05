f26e8c62ee47f3dbb3a781e34f5ff798bb3312f9
ParthaI <partha@turbot.com>
2021-08-05 21:59:06 +0530

    Added ; at the end of the query

--- azure-test/tests/azure_key_vault_managed_hardware_security_module/test-turbot-query.sql
+++ azure-test/tests/azure_key_vault_managed_hardware_security_module/test-turbot-query.sql
@@ -1,3 +1,3 @@
 select name, akas, title, tags
 from azure.azure_key_vault_managed_hardware_security_module
-where name = '{{resourceName}}' and resource_group = '{{resourceName}}'
+where name = '{{resourceName}}' and resource_group = '{{resourceName}}';
