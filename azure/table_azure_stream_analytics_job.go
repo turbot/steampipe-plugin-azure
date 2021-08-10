cb7339b0b4fd124bb1c5769cf58af58e9c406a31
bigdatasourav <bigdatasourav@gmail.com>
2021-08-10 12:45:58 +0530

    WIP

---
+++ azure/table_azure_stream_analytics_job.go
@@ -0,0 +1,201 @@
+package azure
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/services/streamanalytics/mgmt/2016-03-01/streamanalytics"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+)
+
+//// TABLE DEFINITION
+
+func tableAzureStreamAnalyticsJob(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "azure_stream_analytics_job",
+		Description: "Azure Stream Analytics Job",
+		Get: &plugin.GetConfig{
+			KeyColumns:        plugin.AllColumns([]string{"name", "resource_group"}),
+			Hydrate:           getStreamAnalyticsJob,
+			ShouldIgnoreError: isNotFoundError([]string{"ResourceNotFound", "ResourceGroupNotFound", "Invalid input"}),
+		},
+		List: &plugin.ListConfig{
+			Hydrate: listStreamAnalyticsJobs,
+		},
+		Columns: []*plugin.Column{
+			{
+				Name:        "name",
+				Description: "The resource name.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "id",
+				Description: "The resource identifier.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromGo(),
+			},
+			{
+				Name:        "type",
+				Description: "The resource type.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "version",
+				Description: "Version of the factory.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("FactoryProperties.Version"),
+			},
+			{
+				Name:        "create_time",
+				Description: "Specifies the time, the factory was created.",
+				Type:        proto.ColumnType_TIMESTAMP,
+				Transform:   transform.FromField("FactoryProperties.CreateTime").Transform(convertDateToTime),
+			},
+			{
+				Name:        "etag",
+				Description: "An unique read-only string that changes whenever the resource is updated.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ETag"),
+			},
+			{
+				Name:        "provisioning_state",
+				Description: "Factory provisioning state, example Succeeded.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("FactoryProperties.ProvisioningState"),
+			},
+			{
+				Name:        "public_network_access",
+				Description: "Whether or not public network access is allowed for the data factory.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("FactoryProperties.PublicNetworkAccess").Transform(transform.ToString),
+			},
+			{
+				Name:        "additional_properties",
+				Description: "Unmatched properties from the message are deserialized this collection.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "identity",
+				Description: "Managed service identity of the factory.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "encryption",
+				Description: "Properties to enable Customer Managed Key for the factory.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("FactoryProperties.EncryptionConfiguration"),
+			},
+			{
+				Name:        "repo_configuration",
+				Description: "Git repo information of the factory.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("FactoryProperties.RepoConfiguration"),
+			},
+			{
+				Name:        "global_parameters",
+				Description: "List of parameters for factory.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("FactoryProperties.GlobalParameters"),
+			},
+
+			// Steampipe standard columns
+			{
+				Name:        "title",
+				Description: ColumnDescriptionTitle,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Name"),
+			},
+			{
+				Name:        "tags",
+				Description: ColumnDescriptionTags,
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "akas",
+				Description: ColumnDescriptionAkas,
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ID").Transform(idToAkas),
+			},
+
+			// Azure standard column
+			{
+				Name:        "region",
+				Description: ColumnDescriptionRegion,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Location").Transform(toLower),
+			},
+			{
+				Name:        "resource_group",
+				Description: ColumnDescriptionResourceGroup,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID").Transform(extractResourceGroupFromID),
+			},
+			{
+				Name:        "subscription_id",
+				Description: ColumnDescriptionSubscription,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID").Transform(idToSubscriptionID),
+			},
+		},
+	}
+}
+
+//// LIST FUNCTION
+
+func listStreamAnalyticsJobs(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+
+	streamingJobsClient := streamanalytics.NewStreamingJobsClient(subscriptionID)
+	streamingJobsClient.Authorizer = session.Authorizer
+
+	pagesLeft := true
+	for pagesLeft {
+		result, err := streamingJobsClient.List(context.Background(), "")
+		if err != nil {
+			return nil, err
+		}
+		for _, streamingJob := range result.Values() {
+			d.StreamListItem(ctx, streamingJob)
+		}
+		result.NextWithContext(context.Background())
+		pagesLeft = result.NotDone()
+	}
+	return nil, err
+}
+
+//// HYDRATE FUNCTIONS
+
+func getStreamAnalyticsJob(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getStreamAnalyticsJob")
+
+	// Create session
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+	subscriptionID := session.SubscriptionID
+
+	streamingJobsClient := streamanalytics.NewStreamingJobsClient(subscriptionID)
+	streamingJobsClient.Authorizer = session.Authorizer
+
+	name := d.KeyColumnQuals["name"].GetStringValue()
+	resourceGroup := d.KeyColumnQuals["resource_group"].GetStringValue()
+
+	// Return nil, if no input provide
+	if name == "" || resourceGroup == "" {
+		return nil, nil
+	}
+
+	op, err := streamingJobsClient.Get(ctx, resourceGroup, name, "")
+	if err != nil {
+		return nil, err
+	}
+
+	return op, nil
+}
