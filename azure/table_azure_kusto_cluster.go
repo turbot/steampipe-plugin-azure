d5d65b4cc713ff508b808c121b1be1276171bd91
ParthaI <partha@turbot.com>
2021-10-04 16:55:44 +0530

    Add encryption properties in azure_kusto_cluster table closes #373

--- azure/table_azure_kusto_cluster.go
+++ azure/table_azure_kusto_cluster.go
@@ -70,6 +70,36 @@ func tableAzureKustoCluster(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_STRING,
 			},
 			{
+				Name:        "enable_disk_encryption",
+				Description: "A boolean value that indicates if the cluster's disks are encrypted.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("ClusterProperties.EnableDiskEncryption"),
+			},
+			{
+				Name:        "enable_double_encryption",
+				Description: "A boolean value that indicates if double encryption is enabled.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("ClusterProperties.EnableDoubleEncryption"),
+			},
+			{
+				Name:        "enable_purge",
+				Description: "A boolean value that indicates if the purge operations are enabled.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("ClusterProperties.EnablePurge"),
+			},
+			{
+				Name:        "enable_streaming_ingest",
+				Description: "A boolean value that indicates if the streaming ingest is enabled.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("ClusterProperties.EnableStreamingIngest"),
+			},
+			{
+				Name:        "engine_type",
+				Description: "The engine type. Possible values include: 'EngineTypeV2', 'EngineTypeV3'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ClusterProperties.EngineType"),
+			},
+			{
 				Name:        "sku_capacity",
 				Description: "SKU capacity of the resource.",
 				Type:        proto.ColumnType_INT,
@@ -88,16 +118,46 @@ func tableAzureKustoCluster(_ context.Context) *plugin.Table {
 				Transform:   transform.FromField("Sku.Tier"),
 			},
 			{
+				Name:        "state_reason",
+				Description: "SKU tier of the resource.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("SClusterPropertiesku.StateReason"),
+			},
+			{
 				Name:        "uri",
 				Description: "The cluster URI.",
 				Type:        proto.ColumnType_STRING,
 				Transform:   transform.FromField("ClusterProperties.URI"),
 			},
 			{
+				Name:        "language_extensions",
+				Description: "List of the cluster's language extensions.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ClusterProperties.LanguageExtensions"),
+			},
+			{
+				Name:        "key_vault_properties",
+				Description: "KeyVault properties for the cluster encryption.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ClusterProperties.KeyVaultProperties"),
+			},
+			{
+				Name:        "optimized_autoscale",
+				Description: "Optimized auto scale definition.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ClusterProperties.OptimizedAutoscale"),
+			},
+			{
 				Name:        "trusted_external_tenants",
 				Description: "The cluster's external tenants.",
 				Type:        proto.ColumnType_JSON,
 				Transform:   transform.FromField("ClusterProperties.TrustedExternalTenants"),
+			},
+			{
+				Name:        "virtual_network_configuration",
+				Description: "Virtual network definition of the resource.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ClusterProperties.VirtualNetworkConfiguration"),
 			},
 
 			// Steampipe standard columns
