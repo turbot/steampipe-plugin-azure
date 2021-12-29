5381e47380170825393d688669bc75d9a5580898
bigdatasourav <bigdatasourav@gmail.com>
2021-12-29 17:14:57 +0530

    update

--- azure-test/tests/azure_mysql_server/variables.tf
+++ azure-test/tests/azure_mysql_server/variables.tf
@@ -13,7 +13,7 @@ variable "azure_environment" {
 
 variable "azure_subscription" {
   type        = string
-  default     = "d46d7416-f95f-4771-bbb5-529d4c76659c"
+  default     = "3510ae4d-530b-497d-8f30-53b9616fc6c1"
   description = "Azure subscription used for the test."
 }
 
