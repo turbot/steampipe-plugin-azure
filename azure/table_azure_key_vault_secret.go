c3ccd9bc8a1531cfc7d2ee5e26aa2517010468f3
Ved misra <47312748+misraved@users.noreply.github.com>
2022-03-30 18:12:38 +0530

    Update the comments in the code

--- azure/table_azure_key_vault_secret.go
+++ azure/table_azure_key_vault_secret.go
@@ -170,10 +170,10 @@ func listKeyVaultSecrets(ctx context.Context, d *plugin.QueryData, h *plugin.Hyd
 	if err != nil {
 		/*
 		* To make the above API call user must have list secrets permission.
-		* If an user does not have this permission to perform list operation for secrets then the api returns forbiden error.
-		* The permission should be grant in the key valult access policy.
-		* If the forbidden error is not being handled here then it will make the table fail,
-		  if there is at least a single key vault on which user does not have access to perform list secret operation.
+		* If the user does not have this permission then the API returns an unauthorized error.
+		* The permission should be granted in the key valult access policy.
+		* If the unauthorized error is not handled, then it will cause the table to fail
+		  if there is at least a single key vault on which the user does not have access to perform list secret operation.
 		*/
 
 		if strings.Contains(err.Error(), "Forbidden") {
