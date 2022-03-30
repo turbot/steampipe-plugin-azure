3c2f4ce3543fcfdb4c89fdc48838116b1197c8d6
ParthaI <partha@turbot.com>
2022-03-30 10:28:25 +0530

    Updated the comments

--- azure/table_azure_key_vault_secret.go
+++ azure/table_azure_key_vault_secret.go
@@ -169,10 +169,10 @@ func listKeyVaultSecrets(ctx context.Context, d *plugin.QueryData, h *plugin.Hyd
 	result, err := client.GetSecrets(ctx, vaultURI, &maxResults)
 	if err != nil {
 		/*
-		* To make the above API call user must have list secrets grant.
-		* If an user does not have this access to perform list operation for secrets then the api returns forbiden error.
-		* The permission should be grant through the key valult access policy.
-		* If the forbidden error is not being handled here then it will make the table to fail, 
+		* To make the above API call user must have list secrets permission.
+		* If an user does not have this permission to perform list operation for secrets then the api returns forbiden error.
+		* The permission should be grant in the key valult access policy.
+		* If the forbidden error is not being handled here then it will make the table fail, 
 		  if there is at least a single key vault on which user does not have access to perform list secret operation.
 		*/
 
