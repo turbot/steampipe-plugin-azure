2def1ff73f4911e47f036b1f65f7a87abb64a5b6
ParthaI <47887552+ParthaI@users.noreply.github.com>
2022-11-24 20:32:53 +0530

    Add table azure_compute_virtual_machine_scale_set_network_interface closes #530 (#537)
    Co-authored-by: souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

---
+++ azure-test/tests/azure_compute_virtual_machine_scale_set_network_interface/test-turbot-query.sql
@@ -0,0 +1,3 @@
+select title, akas
+from azure_compute_virtual_machine_scale_set_network_interface
+where name = '{{ resourceName }}' and resource_group = '{{ resourceName }}' and id = '{{ output.resource_id.value }}';
--- azure/plugin.go
+++ azure/plugin.go
@@ -59,6 +59,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"azure_compute_virtual_machine_metric_cpu_utilization_daily":  tableAzureComputeVirtualMachineMetricCpuUtilizationDaily(ctx),
 			"azure_compute_virtual_machine_metric_cpu_utilization_hourly": tableAzureComputeVirtualMachineMetricCpuUtilizationHourly(ctx),
 			"azure_compute_virtual_machine_scale_set":                     tableAzureComputeVirtualMachineScaleSet(ctx),
+			"azure_compute_virtual_machine_scale_set_network_interface":   tableAzureComputeVirtualMachineScaleSetNetworkInterface(ctx),
 			"azure_compute_virtual_machine_scale_set_vm":                  tableAzureComputeVirtualMachineScaleSetVm(ctx),
 			"azure_container_registry":                                    tableAzureContainerRegistry(ctx),
 			"azure_cosmosdb_account":                                      tableAzureCosmosDBAccount(ctx),
