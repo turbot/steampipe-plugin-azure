c7ff737dcb3150bccf10236f44f6a2d1552af8ea
ParthaI <47887552+ParthaI@users.noreply.github.com>
2023-02-22 20:31:35 +0530

    Add table azure_automation_account closes #562 (#569)
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

--- azure/plugin.go
+++ azure/plugin.go
@@ -39,6 +39,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"azure_app_service_web_app":                                   tableAzureAppServiceWebApp(ctx),
 			"azure_application_gateway":                                   tableAzureApplicationGateway(ctx),
 			"azure_application_security_group":                            tableAzureApplicationSecurityGroup(ctx),
+			"azure_automation_account":                                    tableAzureApAutomationAccount(ctx),
 			"azure_batch_account":                                         tableAzureBatchAccount(ctx),
 			"azure_cognitive_account":                                     tableAzureCognitiveAccount(ctx),
 			"azure_compute_availability_set":                              tableAzureComputeAvailabilitySet(ctx),
