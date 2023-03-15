0f42dc6c2b10ec28140588dbdeba98bd782ee603
Karan Popat <karan@turbot.com>
2023-03-15 19:13:10 +0530

    Add table `azure_application_insight`. Closes #563 (#579)
    Co-authored-by: misraved <ved@turbot.com>
    Co-authored-by: ParthaI <partha@turbot.com>
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

---
+++ azure-test/tests/azure_application_insight/variables.tf
@@ -0,0 +1,59 @@
+variable "resource_name" {
+  type        = string
+  default     = "turbot-test-20200125-create-update"
+  description = "Name of the resource used throughout the test."
+}
+
+variable "azure_environment" {
+  type        = string
+  default     = "public"
+  description = "Azure environment used for the test."
+}
+
+variable "azure_subscription" {
+  type        = string
+  default     = "d46d7416-f95f-4771-bbb5-529d4c76659c"
+  description = "Azure environment used for the test."
+}
+
+provider "azurerm" {
+  # Cannot be passed as a variable
+  environment     = var.azure_environment
+  subscription_id = var.azure_subscription
+  features {}
+}
+
+data "azuread_client_config" "current" {}
+
+resource "azurerm_resource_group" "named_test_resource" {
+  name     = var.resource_name
+  location = "West US"
+}
+
+resource "azurerm_application_insights" "named_test_resource" {
+  name                = var.resource_name
+  location            = azurerm_resource_group.named_test_resource.location
+  resource_group_name = azurerm_resource_group.named_test_resource.name
+  application_type    = "web"
+
+  tags = {
+    name = var.resource_name
+  }
+}
+
+output "resource_aka" {
+  depends_on = [azurerm_application_insights.named_test_resource]
+  value      = "azure://${azurerm_application_insights.named_test_resource.id}"
+}
+
+output "resource_aka_lower" {
+  value = "azure://${lower(azurerm_application_insights.named_test_resource.id)}"
+}
+
+output "resource_name" {
+  value = var.resource_name
+}
+
+output "resource_id" {
+  value = lower(azurerm_application_insights.named_test_resource.id)
+}
