15679e1a930de7c3cc33f586d1f1bfef3f5c8ce1
Ved misra <47312748+misraved@users.noreply.github.com>
2023-12-12 13:16:19 +0530

    Recompile plugin with steampipe-plugin-sdk v5.8.0 (#699)

--- azure/connection_config.go
+++ azure/connection_config.go
@@ -2,54 +2,19 @@ package azure
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type azureConfig struct {
-	TenantID            *string  `cty:"tenant_id"`
-	SubscriptionID      *string  `cty:"subscription_id"`
-	ClientID            *string  `cty:"client_id"`
-	ClientSecret        *string  `cty:"client_secret"`
-	CertificatePath     *string  `cty:"certificate_path"`
-	CertificatePassword *string  `cty:"certificate_password"`
-	Username            *string  `cty:"username"`
-	Password            *string  `cty:"password"`
-	Environment         *string  `cty:"environment"`
-	IgnoreErrorCodes    []string `cty:"ignore_error_codes"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"tenant_id": {
-		Type: schema.TypeString,
-	},
-	"subscription_id": {
-		Type: schema.TypeString,
-	},
-	"client_id": {
-		Type: schema.TypeString,
-	},
-	"client_secret": {
-		Type: schema.TypeString,
-	},
-	"certificate_path": {
-		Type: schema.TypeString,
-	},
-	"certificate_password": {
-		Type: schema.TypeString,
-	},
-	"username": {
-		Type: schema.TypeString,
-	},
-	"password": {
-		Type: schema.TypeString,
-	},
-	"environment": {
-		Type: schema.TypeString,
-	},
-	"ignore_error_codes": {
-		Type: schema.TypeList,
-		Elem: &schema.Attribute{Type: schema.TypeString},
-	},
+	TenantID            *string  `hcl:"tenant_id"`
+	SubscriptionID      *string  `hcl:"subscription_id"`
+	ClientID            *string  `hcl:"client_id"`
+	ClientSecret        *string  `hcl:"client_secret"`
+	CertificatePath     *string  `hcl:"certificate_path"`
+	CertificatePassword *string  `hcl:"certificate_password"`
+	Username            *string  `hcl:"username"`
+	Password            *string  `hcl:"password"`
+	Environment         *string  `hcl:"environment"`
+	IgnoreErrorCodes    []string `hcl:"ignore_error_codes,optional"`
 }
 
 func ConfigInstance() interface{} {
