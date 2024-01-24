69bea160581b2f8ed0de5a3c7866451e65daeaed
ParthaI <partha@turbot.com>
2024-01-24 19:19:49 +0530

    Add table azure_consumption_usage Closes #668

---
+++ azure/table_azure_consumption_usage.go
@@ -0,0 +1,334 @@
+package azure
+
+import (
+	"context"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/services/consumption/mgmt/2019-10-01/consumption"
+	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
+
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+)
+
+//// TABLE DEFINITION
+
+func tableAzureConsuptionUsage(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "azure_consumption_usage",
+		Description: "Azure Consuption Usage",
+		List: &plugin.ListConfig{
+			Hydrate: listConsuptionUsage,
+			KeyColumns: plugin.KeyColumnSlice{
+				{
+					Name:      "filter",
+					Operators: []string{"="},
+					Require:   plugin.Optional,
+				},
+				{
+					Name:      "metric",
+					Operators: []string{"="},
+					Require:   plugin.Optional,
+				},
+				{
+					Name:      "scope",
+					Operators: []string{"="},
+					Require:   plugin.Optional,
+				},
+				{
+					Name:      "expand",
+					Operators: []string{"="},
+					Require:   plugin.Optional,
+				},
+			},
+		},
+		Columns: azureColumns([]*plugin.Column{
+			{
+				Name:        "name",
+				Type:        proto.ColumnType_STRING,
+				Description: "The ID that uniquely identifies an event.",
+			},
+			{
+				Name:        "id",
+				Description: "The full qualified ARM ID of an event.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("ID"),
+			},
+			{
+				Name:        "scope",
+				Description: "The scope associated with usage details operations.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "metric",
+				Description: "Allows to select different type of cost/usage records. Possible values are 'actualcost', 'amortizedcost' or 'usage'.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromQual("metric"),
+			},
+			{
+				Name:        "filter",
+				Description: "May be used to filter usageDetails by properties/resourceGroup, properties/instanceName, properties/resourceId, properties/chargeType, properties/reservationId, properties/publisherType or tags. The filter supports 'eq', 'lt', 'gt', 'le', 'ge', and 'and'. It does not currently support 'ne', 'or', or 'not'. Tag filter is a key value pair string where key and value is separated by a colon (:). PublisherType Filter accepts two values azure and marketplace and it is currently supported for Web Direct Offer Type.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromQual("filter"),
+			},
+			{
+				Name:        "expand",
+				Description: "May be used to expand the 'properties/additionalInfo' or 'properties/meterDetails' within a list of usage details. By default, these fields are not included when listing usage details.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromQual("expand"),
+			},
+			{
+				Name:        "kind",
+				Description: "Specifies the kind of usage details.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "type",
+				Description: "Type of the resource.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "etag",
+				Description: "The etag for the resource.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "modern_usage_detail",
+				Description: "The modern usage detail.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "legacy_usage_detail",
+				Description: "The legacy usage detail.",
+				Type:        proto.ColumnType_JSON,
+			},
+
+			// Steampipe standard columns
+			{
+				Name:        "title",
+				Description: ColumnDescriptionTitle,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Name"),
+			},
+			{
+				Name:        "tags",
+				Description: ColumnDescriptionTags,
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "akas",
+				Description: ColumnDescriptionAkas,
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("ID").Transform(idToAkas),
+			},
+		}),
+	}
+}
+
+type UsageDetails struct {
+	Scope             *string
+	Kind              consumption.Kind
+	ID                *string
+	Name              *string
+	Type              *string
+	Etag              *string
+	Tags              map[string]*string
+	ModernUsageDetail map[string]interface{}
+	LegacyUsageDetail map[string]interface{}
+}
+
+//// LIST FUNCTION
+
+func listConsuptionUsage(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	if err != nil {
+		return nil, err
+	}
+
+	subscriptionID := session.SubscriptionID
+
+	consumptionClient := consumption.NewUsageDetailsClientWithBaseURI(session.ResourceManagerEndpoint, subscriptionID)
+	consumptionClient.Authorizer = session.Authorizer
+
+	scope := "/subscriptions/" + subscriptionID + "/" // Default scope is subscription
+	if d.EqualsQualString("scope") != "" {
+		scope = d.EqualsQualString("scope")
+	}
+	expand := ""
+	if d.EqualsQualString("expand") != "" {
+		expand = d.EqualsQualString("expand")
+	}
+
+	/**
+	• The API returns an error if a billing time period is not specified for consumption usage.
+	• Error: Billing Period is not supported in (2021-10-01) API Version for Subscription Scope With Web Direct Offer.  Please provide the UsageStart and UsageEnd dates in the $filter key as parameters.
+	• For consumption queries, specifying the start and end dates is required.
+	• By default, the time period considered is the past year.
+	**/
+
+	filter := getConsumptionFilter(d.Quals)
+	skiptoken := ""
+	// top := int32(0)
+	// metric := consumption.MetrictypeUsageMetricType
+	var metric consumption.Metrictype
+	if d.EqualsQualString("metric") != "" {
+		switch d.EqualsQualString("metric") {
+		case "actualcost":
+			metric = consumption.MetrictypeActualCostMetricType
+		case "amortizedcost":
+			metric = consumption.MetrictypeAmortizedCostMetricType
+		case "usage":
+			metric = consumption.MetrictypeUsageMetricType
+		}
+	}
+	result, err := consumptionClient.List(ctx, scope, expand, filter, skiptoken, nil, metric)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, res := range result.Values() {
+		result := getUsageDetailsByUsageDetailKind(res, scope)
+		if result != nil {
+			d.StreamListItem(ctx, result)
+
+			// Check if context has been cancelled or if the limit has been hit (if specified)
+			// if there is a limit, it will return the number of rows required to reach this limit
+			if d.RowsRemaining(ctx) == 0 {
+				return nil, nil
+			}
+		}
+
+	}
+
+	for result.NotDone() {
+		err = result.NextWithContext(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, res := range result.Values() {
+			result := getUsageDetailsByUsageDetailKind(res, scope)
+			if result != nil {
+				d.StreamListItem(ctx, result)
+
+				// Check if context has been cancelled or if the limit has been hit (if specified)
+				// if there is a limit, it will return the number of rows required to reach this limit
+				if d.RowsRemaining(ctx) == 0 {
+					return nil, nil
+				}
+			}
+		}
+	}
+
+	return nil, err
+}
+
+//// UTILITY FUNCTION
+
+// Get ussage details for all type(madern, legacy, basic) of consumption usage.
+func getUsageDetailsByUsageDetailKind(res consumption.BasicUsageDetail, scope string) *UsageDetails {
+	result := &UsageDetails{}
+	modernUsageDetails, isModern := res.AsModernUsageDetail()
+	legacyUsageDetails, isLegacy := res.AsLegacyUsageDetail()
+	usageDetails, ud := res.AsUsageDetail()
+	if isModern {
+		result.Scope = &scope
+		result.ID = modernUsageDetails.ID
+		result.Etag = modernUsageDetails.Etag
+		result.Name = modernUsageDetails.Name
+		result.Tags = modernUsageDetails.Tags
+		result.Type = modernUsageDetails.Type
+		result.Kind = modernUsageDetails.Kind
+		result.ModernUsageDetail = extractUsageDetailProperties(modernUsageDetails.ModernUsageDetailProperties)
+	}
+	if isLegacy {
+		result.Scope = &scope
+		result.ID = legacyUsageDetails.ID
+		result.Etag = legacyUsageDetails.Etag
+		result.Name = legacyUsageDetails.Name
+		result.Tags = legacyUsageDetails.Tags
+		result.Type = legacyUsageDetails.Type
+		result.Kind = legacyUsageDetails.Kind
+		result.LegacyUsageDetail = extractUsageDetailProperties(legacyUsageDetails.LegacyUsageDetailProperties)
+	}
+	if ud {
+		result.Scope = &scope
+		result.ID = usageDetails.ID
+		result.Etag = usageDetails.Etag
+		result.Name = usageDetails.Name
+		result.Tags = usageDetails.Tags
+		result.Type = usageDetails.Type
+		result.Kind = usageDetails.Kind
+	}
+
+	return result
+}
+
+func extractUsageDetailProperties(value interface{}) map[string]interface{} {
+	data := make(map[string]interface{})
+
+	switch item := value.(type) {
+	case *consumption.ModernUsageDetailProperties:
+		if item != nil {
+
+			// Use reflection to iterate over the struct fields
+			data = structToMap(reflect.ValueOf(*item))
+		}
+	case *consumption.LegacyUsageDetailProperties:
+		if item != nil {
+
+			// Use reflection to iterate over the struct fields
+			data = structToMap(reflect.ValueOf(*item))
+		}
+	}
+
+	return data
+}
+
+func structToMap(val reflect.Value) map[string]interface{} {
+	result := make(map[string]interface{})
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Type().Field(i)
+		fieldValue := val.Field(i)
+
+		// Check if field is a struct and not a zero value
+		if fieldValue.Kind() == reflect.Struct && !fieldValue.IsZero() {
+			result[field.Name] = structToMap(fieldValue)
+		} else if !fieldValue.IsZero() {
+			result[field.Name] = fieldValue.Interface()
+		} else {
+			result[field.Name] = nil
+		}
+	}
+
+	return result
+}
+
+func getConsumptionFilter(quals plugin.KeyColumnQualMap) (filter string) {
+	filter = ""
+	if quals["filter"] != nil {
+		for _, q := range quals["filter"].Quals {
+			if q.Operator == "=" {
+				val := q.Value.GetStringValue()
+				filter = val
+			}
+		}
+	}
+
+	outputLayout := "2006-01-02T15:04:05Z"
+	endData := time.Now().AddDate(-1, 0, 0).Format(outputLayout)
+	startData := time.Now().Format(outputLayout)
+
+	// Default time period is last one year
+	if filter != "" && !strings.Contains(filter, "properties/usageEnd") && !strings.Contains(filter, "properties/usageStart") {
+		filter = filter + " and properties/usageEnd eq '" + endData + "' and properties/usageStart eq '" + startData + "'"
+	}
+	if filter == "" && !strings.Contains(filter, "properties/usageEnd") && !strings.Contains(filter, "properties/usageStart") {
+		filter = "properties/usageEnd eq '" + endData + "' and properties/usageStart eq '" + startData + "'"
+	}
+
+	return filter
+}
---
+++ docs/tables/azure_consumption_usage.md
@@ -0,0 +1,223 @@
+---
+title: "Steampipe Table: azure_consumption_usage - Query Azure Cost Management using SQL"
+description: "Allows users to query Azure Cost Management usage details for the defined scope."
+---
+
+# Table: azure_consumption_usage - Query Azure Container Groups using SQL
+
+Azure Consumption Usage provide the ability to explore cost and usage data via multidimensional analysis, where creating customized filters and expressions allow you to answer consumption-related questions for your Azure resources.
+
+## Table Usage Guide
+
+The `azure_consumption_usage` table provides the comprehensive data about the resources and services you have used in your Azure subscription, along with the associated costs. This information is crucial for managing and optimizing Azure costs, understanding billing, and monitoring resource utilization.
+
+**Important notes:**
+- By default this table returns the result for subscription scope.
+- This table can provide consumption usage details for the previous one year.
+- For improved performance, it is advised that you use the optional qual `filter` to limit the result set to a specific time period .
+- This table supports optional quals. Queries with optional quals are optimized to use Consumption Usage filters. Optional quals are supported for the following columns:
+  - `filter`: May be used to filter usageDetails by properties/resourceGroup, properties/instanceName, properties/resourceId, properties/chargeType, properties/reservationId, properties/publisherType or tags. The filter supports 'eq', 'lt', 'gt', 'le', 'ge', and 'and'. It does not currently support 'ne', 'or', or 'not'. Tag filter is a key value pair string where key and value is separated by a colon (:). PublisherType Filter accepts two values azure and marketplace and it is currently supported for Web Direct Offer Type."
+  - `metric`: Allows to select different type of cost/usage records. Possible values are 'actualcost', 'amortizedcost' or 'usage'.
+  - `scope`: The scope associated with usage details operations. This includes '/subscriptions/{subscriptionId}/' for subscription scope, '/providers/Microsoft.Billing/billingAccounts/{billingAccountId}' for Billing Account scope, '/providers/Microsoft.Billing/departments/{departmentId}' for Department scope, '/providers/Microsoft.Billing/enrollmentAccounts/{enrollmentAccountId}' for EnrollmentAccount scope and '/providers/Microsoft.Management/managementGroups/{managementGroupId}' for Management Group scope. For subscription, billing account, department, enrollment account and management group, you can also add billing period to the scope using '/providers/Microsoft.Billing/billingPeriods/{billingPeriodName}'. For e.g. to specify billing period at department scope use '/providers/Microsoft.Billing/departments/{departmentId}/providers/Microsoft.Billing/billingPeriods/{billingPeriodName}'. Also, Modern Commerce Account scopes are '/providers/Microsoft.Billing/billingAccounts/{billingAccountId}' for billingAccount scope, '/providers/Microsoft.Billing/billingAccounts/{billingAccountId}/billingProfiles/{billingProfileId}' for billingProfile scope, 'providers/Microsoft.Billing/billingAccounts/{billingAccountId}/billingProfiles/{billingProfileId}/invoiceSections/{invoiceSectionId}' for invoiceSection scope, and 'providers/Microsoft.Billing/billingAccounts/{billingAccountId}/customers/{customerId}' specific for partners.
+  - `expand`: May be used to expand the 'properties/additionalInfo' or 'properties/meterDetails' within a list of usage details. By default, these fields are not included when listing usage details.
+
+## Examples
+
+### Basic info
+This query is useful for getting a broad overview of resource consumption in Azure. It can help in cost management, resource optimization, and understanding how different Azure resources are being utilized. The data retrieved can be instrumental for in-depth analysis, especially when dealing with complex Azure environments with multiple resources and services.
+
+```sql+postgres
+select
+  name,
+  id,
+  scope,
+  kind,
+  etag,
+  type
+from
+  azure_consumption_usage;
+```
+
+```sql+sqlite
+select
+  name,
+  id,
+  scope,
+  kind,
+  etag,
+  type
+from
+  azure_consumption_usage;
+```
+
+### Get legacy consumption usage in a subscription
+ This is beneficial for organizations looking to get insights into their legacy resource usage in Azure, aiding in decision-making regarding migration, cost management, and resource optimization.
+
+```sql+postgres
+select
+  name,
+  id,
+  scope,
+  kind,
+  etag,
+  type
+from
+  azure_consumption_usage
+where
+  kind = 'legacy';
+```
+
+```sql+sqlite
+select
+  name,
+  id,
+  scope,
+  kind,
+  etag,
+  type
+from
+  azure_consumption_usage
+where
+  kind = 'legacy';
+```
+
+### Filter actual cost stastics of legacy consumption usage
+Extract detailed insights into their Azure consumption, focusing on legacy resources and actual cost metrics. It aids in financial management, resource optimization, and strategic planning in the context of Azure cloud services.
+
+```sql+postgres
+select
+  name,
+  id,
+  metric,
+  kind,
+  legacy_usage_detail ->> 'BillingAccountID' as billing_account_id,
+  legacy_usage_detail ->> 'BillingAccountName' as billing_account_name,
+  legacy_usage_detail ->> 'BillingPeriodStartDate' as billing_period_start_date,
+  legacy_usage_detail ->> 'BillingPeriodEndDate' as billing_period_end_date,
+  legacy_usage_detail ->> 'Product' as product,
+  legacy_usage_detail ->> 'Quantity' as quantity,
+  legacy_usage_detail ->> 'Cost' as cost,
+  legacy_usage_detail ->> 'BillingCurrency' as billing_currency,
+  legacy_usage_detail ->> 'ChargeType' as charge_type,
+  legacy_usage_detail ->> 'IsAzureCreditEligible' as is_azure_credit_eligible,
+  legacy_usage_detail ->> 'ResourceID' as resource_id
+from
+  azure_consumption_usage;
+where
+  kind = 'legacy'
+  and metric = 'actualcost';
+```
+
+```sql+sqlite
+select
+  name,
+  id,
+  metric,
+  kind,
+  json_extract(legacy_usage_detail, '$.BillingAccountID') as billing_account_id,
+  json_extract(legacy_usage_detail, '$.BillingAccountName') as billing_account_name,
+  json_extract(legacy_usage_detail, '$.BillingPeriodStartDate') as billing_period_start_date,
+  json_extract(legacy_usage_detail, '$.BillingPeriodEndDate') as billing_period_end_date,
+  json_extract(legacy_usage_detail, '$.Product') as product,
+  json_extract(legacy_usage_detail, '$.Quantity') as quantity,
+  json_extract(legacy_usage_detail, '$.Cost') as cost,
+  json_extract(legacy_usage_detail, '$.BillingCurrency') as billing_currency,
+  json_extract(legacy_usage_detail, '$.ChargeType') as charge_type,
+  json_extract(legacy_usage_detail, '$.IsAzureCreditEligible') as is_azure_credit_eligible,
+  json_extract(legacy_usage_detail, '$.ResourceID') as resource_id
+from azure_consumption_usage
+where
+  kind = 'legacy'
+  and metric = 'actualcost';
+```
+
+### Get top 10 legacy consumption usages in a year
+Analyze the distribution of Azure container groups based on their operating system type. This can help in understanding the usage pattern of different OS types within your Azure container groups.
+
+```sql+postgres
+select
+  name,
+  id,
+  metric,
+  kind,
+  legacy_usage_detail ->> 'BillingAccountID' as billing_account_id,
+  legacy_usage_detail ->> 'BillingAccountName' as billing_account_name,
+  legacy_usage_detail ->> 'BillingPeriodStartDate' as billing_period_start_date,
+  legacy_usage_detail ->> 'BillingPeriodEndDate' as billing_period_end_date,
+  legacy_usage_detail ->> 'Cost' as cost,
+  legacy_usage_detail ->> 'BillingCurrency' as billing_currency
+from
+  azure_consumption_usage
+where
+  kind = 'legacy'
+and
+  metric = 'actualcost'
+order by
+  cost desc limit 10;
+```
+
+```sql+sqlite
+select
+  name,
+  id,
+  metric,
+  kind,
+  json_extract(legacy_usage_detail, '$.BillingAccountID') as billing_account_id,
+  json_extract(legacy_usage_detail, '$.BillingAccountName') as billing_account_name,
+  json_extract(legacy_usage_detail, '$.BillingPeriodStartDate') as billing_period_start_date,
+  json_extract(legacy_usage_detail, '$.BillingPeriodEndDate') as billing_period_end_date,
+  json_extract(legacy_usage_detail, '$.Cost') as cost,
+  json_extract(legacy_usage_detail, '$.BillingCurrency') as billing_currency
+from
+  azure_consumption_usage
+where
+  kind = 'legacy'
+and
+  metric = 'actualcost'
+order by
+  cost desc limit 10;
+```
+
+### Filter consumption usage by resource group
+Discover the segments that provide information about IP addresses associated with each group. This is useful in understanding the network connectivity and accessibility of these groups within the Azure container ecosystem.
+
+```sql+postgres
+select
+  name,
+  id,
+  metric,
+  kind,
+  legacy_usage_detail ->> 'BillingAccountID' as billing_account_id,
+  legacy_usage_detail ->> 'BillingAccountName' as billing_account_name,
+  legacy_usage_detail ->> 'BillingPeriodStartDate' as billing_period_start_date,
+  legacy_usage_detail ->> 'BillingPeriodEndDate' as billing_period_end_date,
+  legacy_usage_detail ->> 'Cost' as cost,
+  legacy_usage_detail ->> 'BillingCurrency' as billing_currency,
+  legacy_usage_detail ->> 'ResourceID' as resource_id
+from
+  azure_consumption_usage
+where
+  kind = 'legacy'
+  and metric = 'actualcost'
+  and filter = 'properties/resourceGroup eq ''turbot_rg''';
+```
+
+```sql+sqlite
+select
+  name,
+  id,
+  metric,
+  kind,
+  json_extract(legacy_usage_detail, '$.BillingAccountID') as billing_account_id,
+  json_extract(legacy_usage_detail, '$.BillingAccountName') as billing_account_name,
+  json_extract(legacy_usage_detail, '$.BillingPeriodStartDate') as billing_period_start_date,
+  json_extract(legacy_usage_detail, '$.BillingPeriodEndDate') as billing_period_end_date,
+  json_extract(legacy_usage_detail, '$.Cost') as cost,
+  json_extract(legacy_usage_detail, '$.BillingCurrency') as billing_currency
+from
+  azure_consumption_usage
+where
+  kind = 'legacy'
+  and metric = 'actualcost'
+  and filter = 'properties/resourceGroup eq ''turbot_rg''';
+```
