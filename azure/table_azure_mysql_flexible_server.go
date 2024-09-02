a7dbee83f645a2af45457259f216480e90155800
ParthaI <47887552+ParthaI@users.noreply.github.com>
2024-09-02 12:41:47 +0530

    Update the table azure_mysql_flexible_server to use the Azure ARM Go package Closes #819 (#820)

--- azure/table_azure_mysql_flexible_server.go
+++ azure/table_azure_mysql_flexible_server.go
@@ -9,7 +9,7 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/resources/mgmt/resources"
-	"github.com/Azure/azure-sdk-for-go/profiles/latest/mysql/mgmt/mysqlflexibleservers"
+	armmysqlflexibleservers "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mysql/armmysqlflexibleservers/v2"
 )
 
 //// TABLE DEFINITION
@@ -50,55 +50,55 @@ func tableAzureMySQLFlexibleServer(_ context.Context) *plugin.Table {
 				Name:        "state",
 				Description: "The state of the server.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.State"),
+				Transform:   transform.FromField("Properties.State"),
 			},
 			{
 				Name:        "version",
 				Description: "Specifies the version of the server.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.Version").Transform(transform.ToString),
+				Transform:   transform.FromField("Properties.Version"),
 			},
 			{
 				Name:        "administrator_login",
 				Description: "The administrator's login name of a server.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.AdministratorLogin"),
+				Transform:   transform.FromField("Properties.AdministratorLogin"),
 			},
 			{
 				Name:        "availability_zone",
 				Description: "Availability Zone information of the server.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.AvailabilityZone"),
+				Transform:   transform.FromField("Properties.AvailabilityZone"),
 			},
 			{
 				Name:        "backup_retention_days",
 				Description: "Backup retention days for the server.",
 				Type:        proto.ColumnType_INT,
-				Transform:   transform.FromField("ServerProperties.Backup.BackupRetentionDays"),
+				Transform:   transform.FromField("Properties.Backup.BackupRetentionDays"),
 			},
 			{
 				Name:        "create_mode",
 				Description: "The mode to create a new server.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.CreateMode"),
+				Transform:   transform.FromField("Properties.CreateMode"),
 			},
 			{
 				Name:        "earliest_restore_date",
 				Description: "Specifies the earliest restore point creation time.",
 				Type:        proto.ColumnType_TIMESTAMP,
-				Transform:   transform.FromField("ServerProperties.Backup.EarliestRestoreDate").Transform(convertDateToTime),
+				Transform:   transform.FromField("Properties.Backup.EarliestRestoreDate"),
 			},
 			{
 				Name:        "fully_qualified_domain_name",
 				Description: "The fully qualified domain name of the server.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.FullyQualifiedDomainName"),
+				Transform:   transform.FromField("Properties.FullyQualifiedDomainName"),
 			},
 			{
 				Name:        "geo_redundant_backup",
 				Description: "Indicates whether Geo-redundant is enabled, or not for server backup.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.Backup.GeoRedundantBackup").Transform(transform.ToString),
+				Transform:   transform.FromField("Properties.Backup.GeoRedundantBackup"),
 			},
 			{
 				Name:        "location",
@@ -109,67 +109,67 @@ func tableAzureMySQLFlexibleServer(_ context.Context) *plugin.Table {
 				Name:        "public_network_access",
 				Description: "Whether or not public network access is allowed for this server.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.Network.PublicNetworkAccess").Transform(transform.ToString),
+				Transform:   transform.FromField("Properties.Network.PublicNetworkAccess"),
 			},
 			{
 				Name:        "replica_capacity",
 				Description: "The maximum number of replicas that a primary server can have.",
 				Type:        proto.ColumnType_INT,
-				Transform:   transform.FromField("ServerProperties.ReplicaCapacity"),
+				Transform:   transform.FromField("Properties.ReplicaCapacity"),
 			},
 			{
 				Name:        "replication_role",
 				Description: "The replication role of the server.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.ReplicationRole"),
+				Transform:   transform.FromField("Properties.ReplicationRole"),
 			},
 			{
 				Name:        "restore_point_in_time",
 				Description: "Restore point creation time (ISO8601 format), specifying the time to restore from.",
 				Type:        proto.ColumnType_TIMESTAMP,
-				Transform:   transform.FromField("ServerProperties.RestorePointInTime"),
+				Transform:   transform.FromField("Properties.RestorePointInTime"),
 			},
 			{
 				Name:        "sku_name",
 				Description: "The name of the sku.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("Sku.Name"),
+				Transform:   transform.FromField("SKU.Name"),
 			},
 			{
 				Name:        "sku_tier",
 				Description: "The tier of the particular SKU.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("Sku.Tier").Transform(transform.ToString),
+				Transform:   transform.FromField("SKU.Tier"),
 			},
 			{
 				Name:        "source_server_resource_id",
 				Description: "The source MySQL server id.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.SourceServerResourceID"),
+				Transform:   transform.FromField("Properties.SourceServerResourceID"),
 			},
 			{
 				Name:        "storage_auto_grow",
 				Description: "Indicates whether storage auto grow is enabled, or not.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.Storage.AutoGrow").Transform(transform.ToString),
+				Transform:   transform.FromField("Properties.Storage.AutoGrow"),
 			},
 			{
 				Name:        "storage_iops",
 				Description: "Storage IOPS for a server.",
 				Type:        proto.ColumnType_INT,
-				Transform:   transform.FromField("ServerProperties.Storage.Iops"),
+				Transform:   transform.FromField("Properties.Storage.Iops"),
 			},
 			{
 				Name:        "storage_size_gb",
 				Description: "Indicates max storage allowed for a server.",
 				Type:        proto.ColumnType_INT,
-				Transform:   transform.FromField("ServerProperties.Storage.StorageSizeGB"),
+				Transform:   transform.FromField("Properties.Storage.StorageSizeGB"),
 			},
 			{
 				Name:        "storage_sku",
 				Description: "The sku name of the server storage.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("ServerProperties.Storage.StorageSku"),
+				Transform:   transform.FromField("Properties.Storage.StorageSKU"),
 			},
 			{
 				Name:        "flexible_server_configurations",
@@ -182,19 +182,19 @@ func tableAzureMySQLFlexibleServer(_ context.Context) *plugin.Table {
 				Name:        "high_availability",
 				Description: "High availability related properties of a server.",
 				Type:        proto.ColumnType_JSON,
-				Transform:   transform.FromField("ServerProperties.HighAvailability"),
+				Transform:   transform.FromField("Properties.HighAvailability"),
 			},
 			{
 				Name:        "network",
 				Description: "Network related properties of a server.",
 				Type:        proto.ColumnType_JSON,
-				Transform:   transform.FromField("ServerProperties.Network"),
+				Transform:   transform.FromField("Properties.Network"),
 			},
 			{
 				Name:        "maintenance_window",
 				Description: "Maintenance window of a server.",
 				Type:        proto.ColumnType_JSON,
-				Transform:   transform.FromField("ServerProperties.MaintenanceWindow"),
+				Transform:   transform.FromField("Properties.MaintenanceWindow"),
 			},
 			{
 				Name:        "system_data",
@@ -241,39 +241,32 @@ func tableAzureMySQLFlexibleServer(_ context.Context) *plugin.Table {
 //// LIST FUNCTION
 
 func listMySQLFlexibleServers(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	session, err := GetNewSessionUpdated(ctx, d)
 	if err != nil {
 		return nil, err
 	}
 	subscriptionID := session.SubscriptionID
 
-	client := mysqlflexibleservers.NewServersClientWithBaseURI(session.ResourceManagerEndpoint, subscriptionID)
-	client.Authorizer = session.Authorizer
+	client, err := armmysqlflexibleservers.NewServersClient(subscriptionID, session.Cred, nil)
+	if err != nil {
+		plugin.Logger(ctx).Error("azure_mysql_flexible_server.listMySQLFlexibleServers", "session_error", err)
+		return nil, err
+	}
+	// client.Authorizer = session.Authorizer
 
 	resourceGroupName := h.Item.(resources.Group).Name
 
-	result, err := client.ListByResourceGroup(ctx, *resourceGroupName)
-	if err != nil {
-		plugin.Logger(ctx).Error("listMySQLFlexibleServers", "list", err)
-		return nil, err
-	}
+	pager := client.NewListByResourceGroupPager(*resourceGroupName, nil)
 
-	for _, server := range result.Values() {
-		d.StreamListItem(ctx, server)
-		// Check if context has been cancelled or if the limit has been hit (if specified)
-		// if there is a limit, it will return the number of rows required to reach this limit
-		if d.RowsRemaining(ctx) == 0 {
-			return nil, nil
-		}
-	}
-
-	for result.NotDone() {
-		err = result.NextWithContext(ctx)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
 		if err != nil {
+			plugin.Logger(ctx).Error("azure_mysql_flexible_server.listMySQLFlexibleServers", "api_error", err)
 			return nil, err
 		}
-		for _, server := range result.Values() {
-			d.StreamListItem(ctx, server)
+
+		for _, item := range page.Value {
+			d.StreamListItem(ctx, *item)
 			// Check if context has been cancelled or if the limit has been hit (if specified)
 			// if there is a limit, it will return the number of rows required to reach this limit
 			if d.RowsRemaining(ctx) == 0 {
@@ -288,7 +281,6 @@ func listMySQLFlexibleServers(ctx context.Context, d *plugin.QueryData, h *plugi
 //// HYDRATE FUNCTIONS
 
 func getMySQLFlexibleServer(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	plugin.Logger(ctx).Trace("getMySQLFlexibleServer")
 
 	name := d.EqualsQuals["name"].GetStringValue()
 	resourceGroup := d.EqualsQuals["resource_group"].GetStringValue()
@@ -298,56 +290,69 @@ func getMySQLFlexibleServer(ctx context.Context, d *plugin.QueryData, h *plugin.
 		return nil, nil
 	}
 
-	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	session, err := GetNewSessionUpdated(ctx, d)
 	if err != nil {
+		plugin.Logger(ctx).Error("azure_mysql_flexible_server.getMySQLFlexibleServer", "session_error", err)
 		return nil, err
 	}
+
 	subscriptionID := session.SubscriptionID
 
-	client := mysqlflexibleservers.NewServersClientWithBaseURI(session.ResourceManagerEndpoint, subscriptionID)
-	client.Authorizer = session.Authorizer
-
-	op, err := client.Get(ctx, resourceGroup, name)
+	client, err := armmysqlflexibleservers.NewServersClient(subscriptionID, session.Cred, nil)
 	if err != nil {
-		plugin.Logger(ctx).Error("getMySQLFlexibleServer", "get", err)
+		plugin.Logger(ctx).Error("azure_mysql_flexible_server.getMySQLFlexibleServer", "client_error", err)
+		return nil, err
+	}
+
+	op, err := client.Get(ctx, resourceGroup, name, nil)
+	if err != nil {
+		plugin.Logger(ctx).Error("azure_mysql_flexible_server.getMySQLFlexibleServer", "api_error", err)
 		return nil, err
 	}
 
 	// In some cases resource does not give any notFound error
 	// instead of notFound error, it returns empty data
 	if op.ID != nil {
-		return op, nil
+		return op.Server, nil
 	}
 
 	return nil, nil
 }
 
 func listMySQLFlexibleServersConfigurations(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	plugin.Logger(ctx).Trace("listMySQLFlexibleServersConfigurations")
 
-	server := h.Item.(mysqlflexibleservers.Server)
+	server := h.Item.(armmysqlflexibleservers.Server)
 	resourceGroup := strings.Split(string(*server.ID), "/")[4]
 	serverName := *server.Name
 
-	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	session, err := GetNewSessionUpdated(ctx, d)
 	if err != nil {
+		plugin.Logger(ctx).Error("azure_mysql_flexible_server.listMySQLFlexibleServersConfigurations", "session_error", err)
 		return nil, err
 	}
+
 	subscriptionID := session.SubscriptionID
 
-	client := mysqlflexibleservers.NewConfigurationsClientWithBaseURI(session.ResourceManagerEndpoint, subscriptionID)
-	client.Authorizer = session.Authorizer
-
-	op, err := client.ListByServer(ctx, resourceGroup, serverName)
+	client, err := armmysqlflexibleservers.NewConfigurationsClient(subscriptionID, session.Cred, nil)
 	if err != nil {
-		plugin.Logger(ctx).Error("listMySQLFlexibleServersConfigurations", "list", err)
+		plugin.Logger(ctx).Error("azure_mysql_flexible_server.listMySQLFlexibleServersConfigurations", "client_error", err)
 		return nil, err
 	}
+
+	pager := client.NewListByServerPager(resourceGroup, serverName, nil)
 
 	var mySQLFlexibleServersConfigurations []map[string]interface{}
 
-	for _, i := range op.Values() {
-		mySQLFlexibleServersConfigurations = append(mySQLFlexibleServersConfigurations, extractMySQLFlexibleServersconfiguration(i))
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			plugin.Logger(ctx).Error("azure_mysql_flexible_server.listMySQLFlexibleServersConfigurations", "api_error", err)
+			return nil, err
+		}
+
+		for _, item := range page.Value {
+			mySQLFlexibleServersConfigurations = append(mySQLFlexibleServersConfigurations, extractMySQLFlexibleServersconfiguration(item))
+		}
 	}
 
 	return mySQLFlexibleServersConfigurations, nil
@@ -356,7 +361,7 @@ func listMySQLFlexibleServersConfigurations(ctx context.Context, d *plugin.Query
 //// TRANSFORM FUNCTION
 
 // If we return the API response directly, the output will not provide the properties of Configurations
-func extractMySQLFlexibleServersconfiguration(i mysqlflexibleservers.Configuration) map[string]interface{} {
+func extractMySQLFlexibleServersconfiguration(i *armmysqlflexibleservers.Configuration) map[string]interface{} {
 	mySQLFlexibleServersconfiguration := make(map[string]interface{})
 
 	if i.ID != nil {
@@ -368,8 +373,8 @@ func extractMySQLFlexibleServersconfiguration(i mysqlflexibleservers.Configurati
 	if i.Type != nil {
 		mySQLFlexibleServersconfiguration["Type"] = *i.Type
 	}
-	if i.ConfigurationProperties != nil {
-		mySQLFlexibleServersconfiguration["ConfigurationProperties"] = *i.ConfigurationProperties
+	if i.Properties != nil {
+		mySQLFlexibleServersconfiguration["ConfigurationProperties"] = *i.Properties
 	}
 
 	return mySQLFlexibleServersconfiguration
