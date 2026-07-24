855161b83bf7b2900bfc47765f058c1fd4177a8f
Jérémy MARMOL <jeremy.marmol@claranet.com>
2026-07-24 15:50:05 +0200

    fix(azure_resource_graph): 🐛 update query retrieval method

--- azure/table_azure_resource_graph.go
+++ azure/table_azure_resource_graph.go
@@ -149,7 +149,7 @@ func listAzureResourceGraph(ctx context.Context, d *plugin.QueryData, _ *plugin.
 		return nil, err
 	}
 
-	kqlQuery := d.EqualsQuals["query"]
+	kqlQuery := d.EqualsQualString("query")
 	if kqlQuery == "" {
 		return nil, nil
 	}
